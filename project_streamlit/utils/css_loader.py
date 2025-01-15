f4c8c829904eab94444264172a1d5596cb78aea4
gom-jung <110319273+gom-jung@users.noreply.github.com>
2025-01-15 11:02:16 +0900

    [#64][Change]Streamlit 기존 파일 제거
    Streamlit 기존 파일 제거

--- project_streamlit/utils/css_loader.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def load_css(file_path):
-    try:
-        with open(file_path, "r") as f:
-            css_content = f"<style>{f.read()}</style>"
-            st.markdown(css_content, unsafe_allow_html=True)
-    except FileNotFoundError:
-        st.error("CSS 파일이 존재하지 않습니다. 경로를 확인하세요.")
