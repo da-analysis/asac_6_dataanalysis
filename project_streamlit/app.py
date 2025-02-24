af5b35ee175e7450b151fedb5780c2c5143109dd
gom-jung <110319273+gom-jung@users.noreply.github.com>
2025-02-24 13:18:42 +0900

    Add files via upload

--- project_streamlit/app.py
+++ project_streamlit/app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from utils.db_connector import connect_to_databricks
 from utils.font_loader import load_custom_font
 from utils.css_loader import load_css
 from PIL import Image
@@ -7,19 +6,28 @@ from PIL import Image
 # 페이지 설정
 st.set_page_config(page_title="전국 지역화폐 데이터 현황판", page_icon="🪙", layout="wide")
 
-# load css
-load_custom_font()
-load_css("ui/styles.css")
+# CSS 및 폰트 로드
+@st.cache_resource
+def load_styles():
+    load_custom_font()
+    load_css("ui/styles.css")
 
-logo = Image.open('images/logo.png')
-logo2 = Image.open('images/logo2.png')
+load_styles()
+
+# 로고
+@st.cache_resource
+def load_logo_images():
+    return Image.open('images/logo.png'), Image.open('images/logo2.png')
+
+logo, logo2 = load_logo_images()
 
 # 네비게이션 바
 pg = st.navigation([
     st.Page("1_🏠__Main.py"),
-    st.Page("2_🗺️__지역화폐_발행_현황.py"),
-    st.Page("3_📊__대시보드.py"),
-    st.Page("4_🤖__Chatbot.py")
+    st.Page("2_대시보드_조회_신청.py"),
+    st.Page("3_🗺️__지역화폐_발행_현황.py"),
+    st.Page("4_📊__대시보드.py"),
+    st.Page("5_🤖__Chatbot.py")
 ])
 pg.run()
 
